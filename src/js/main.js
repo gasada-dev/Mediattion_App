2bbd4d54f583330c14cdb805c8fa86935fd21171
gasada <gasada@protonmail.com>
2021-03-12 19:21:00 +0300

    feat: add func restart and func select theme

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,33 @@ const main = () => {
   const song = document.querySelector('.song');
   const play = document.querySelector('.play');
   const outline = document.querySelector('.moving-outline circle');
-  const video = document.querySelector('.video');
-  const sounds = document.querySelectorAll('.sound-select .button')
-  const timeDisplay = document.querySelector('.time-display')
+  const video = document.querySelector('.video-container video');
+  const sounds = document.querySelectorAll('.sound-select .button');
+  const timeDisplay = document.querySelector('.time-display');
   const outLineLength = outline.getTotalLength();
-  const timeSelect = document.querySelectorAll('.time-select button')
-
+  const timeSelect = document.querySelectorAll('.time-select button');
+  const restart = document.querySelector('.restart');
 
   let fakeDuration = 600;
 
   outline.style.strokeDashoffset = outLineLength;
   outline.style.strokeDasharray = outLineLength;
 
+  sounds.forEach(sound => {
+    sound.addEventListener('click', function () {
+      song.src = this.getAttribute('data-sound');
+      video.src = this.getAttribute('data-video');
+      checkPlaying(song);
+    });
+  });
+
   play.addEventListener('click', () => {
     checkPlaying(song);
   });
+
+  restart.addEventListener('click', () => restartSong(song));
+
+  const restartSong = song => song.currentTime = 0;
 
   timeSelect.forEach(option => {
     option.addEventListener("click", function () {
